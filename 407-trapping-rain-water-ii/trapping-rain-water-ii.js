1d9d653c481e64919adc761f828b92b68c00fbfc
ahmed1267 <ahmedadel837@gmail.com>
2023-08-14 04:04:32 +0300

    Time: 171 ms (58.62%) | Memory: 55.2 MB (58.62%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.js
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.js
@@ -4,7 +4,7 @@
  */
 var trapRainWater = function(heightMap) {
     if(heightMap.length<3||heightMap[0].length<3) return 0
-    let arr=Array.from({length:heightMap.length},()=> Array.from({length: heightMap[0].length},()=> false))
+    let visited=Array.from({length:heightMap.length},()=> Array.from({length: heightMap[0].length},()=> false))
     let q= new MinPriorityQueue({priority: (x)=> x[2]})
     let total=0
     let dir= [[0,1],[0,-1],[-1,0],[1,0]]
@@ -17,14 +17,14 @@ var trapRainWater = function(heightMap) {
     }
     while(!q.isEmpty()){
         let [i,j,height] = q.dequeue().element
-        arr[i][j]=true
+        visited[i][j]=true
         for(let [dI,dJ] of dir){
-            if(i+dI>=m||i+dI<0||j+dJ>=n||j+dJ<0||arr[i+dI][j+dJ]) continue;
+            if(i+dI>=m||i+dI<0||j+dJ>=n||j+dJ<0||visited[i+dI][j+dJ]) continue;
             if(heightMap[i+dI][j+dJ]<height){
                 total += height-heightMap[i+dI][j+dJ]
                 heightMap[i+dI][j+dJ]=height
             }
-            arr[i+dI][j+dJ]=true
+            visited[i+dI][j+dJ]=true
             q.enqueue([i+dI,j+dJ,heightMap[i+dI][j+dJ]])
         }
     }
