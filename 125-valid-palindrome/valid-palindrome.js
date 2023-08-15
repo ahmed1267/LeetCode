7637dd3c17782e705ce46dd7fb4bc0cc2b075a32
ahmed1267 <ahmedadel837@gmail.com>
2023-08-15 21:57:45 +0300

    Time: 60 ms (88.75%) | Memory: 44.4 MB (77.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -5,12 +5,12 @@
 var isPalindrome = function (s) {
 
     let str = s.replace(/[^a-z0-9]/gi, "").toLowerCase()
-
-    let rightEdge = str.length - 1
-    let leftEdge = 0
     if (!str) {
         return true
     }
+
+    let rightEdge = str.length - 1
+    let leftEdge = 0
     while (rightEdge - leftEdge >= 1) {
         if (str[rightEdge] != str[leftEdge]) {
             return false
