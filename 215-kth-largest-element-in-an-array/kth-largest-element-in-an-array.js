9e589ad12ca77bfad322c540073834c6c4e8a2ad
ahmed1267 <ahmedadel837@gmail.com>
2023-08-17 16:12:38 +0300

    Time: 172 ms (29.25%) | Memory: 70.3 MB (5.08%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -4,13 +4,10 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    // let q= new MaxPriorityQueue()
-    // nums.forEach((num) => q.enqueue(num)) 
-
-    // for(let i=0;i<k-1;i++){
-    //     q.dequeue()
-    // }
-    // return q.front().element
-    nums=nums.sort((a,b)=>b-a)
-    return nums[k-1]
+    let q=new MaxPriorityQueue()
+    nums.forEach((num)=> q.enqueue(num))
+    for(let i=0;i<k-1;i++){
+        q.dequeue()
+    }
+    return q.front().element
 };
