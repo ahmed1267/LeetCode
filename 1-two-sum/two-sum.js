ea1f7c1fadd3ea5afc380492306d00443150bea6
ahmed1267 <ahmedadel837@gmail.com>
2023-08-17 16:50:51 +0300

    Time: 55 ms (90.77%) | Memory: 44.5 MB (12.13%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,12 +5,22 @@
  */
 var twoSum = function (nums, target) {
 
-    for (let i = 0; i < nums.length; i++) {
+    // for (let i = 0; i < nums.length; i++) {
         
-        if(nums.includes(target-nums[i])&& nums.indexOf(target-nums[i])!=i) return [i,nums.indexOf(target-nums[i])]
-    }
-    // nums=nums.sort((a,b)=> a-b)
-    // for(let i=0;i<nums.length;i++){
-
+    //     if(nums.includes(target-nums[i])&& nums.indexOf(target-nums[i])!=i) return [i,nums.indexOf(target-nums[i])]
     // }
+    let newNum=[]
+    for(let i=0;i<nums.length;i++){
+        newNum.push([nums[i],i])
+    }
+    newNum=newNum.sort((a,b)=> a[0]-b[0])
+    let left=0
+    let right=newNum.length-1
+    while(left<right){
+        let sum =newNum[left][0]+newNum[right][0]
+        if(sum==target) return[newNum[left][1],newNum[right][1]]
+        else if(sum>target) right--
+        else if(sum<target) left++
+        
+    }
 };
