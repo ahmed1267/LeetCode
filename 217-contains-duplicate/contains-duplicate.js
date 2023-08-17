89ac3099b7021a8d37f2c86fcbd228bce034e3f3
ahmed1267 <ahmedadel837@gmail.com>
2023-08-17 17:02:40 +0300

    Time: 79 ms (85.19%) | Memory: 54.5 MB (47.69%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -4,16 +4,13 @@
  */
 var containsDuplicate = function(nums) {
     
-    let map = new Map()
-    for(let i =0; i<nums.length;i++){
-        if(!map.get(nums[i])) map.set(nums[i],1)
-        else return true
-    }
-    return false
-    // let set=new Set()
-    // for(let i=0;i<nums.length;i++){
-    //     set.add(nums[i])
+    // let map = new Map()
+    // for(let i =0; i<nums.length;i++){
+    //     if(!map.get(nums[i])) map.set(nums[i],1)
+    //     else return true
     // }
-    // if(set.size!=nums.length) return true
     // return false
+    let set=new Set(nums)
+    if(set.size!=nums.length) return true
+    return false
 };
