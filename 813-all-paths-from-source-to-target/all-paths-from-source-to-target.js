10932794846a0baa15466819182a77ecadae479d
ahmed1267 <ahmedadel837@gmail.com>
2023-08-19 13:09:24 +0300

    Time: 95 ms (59.03%) | Memory: 49.6 MB (55.80%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+    let res=[]
+    var dfs= function(node,path){
+        if(node==graph.length-1){
+            res.push(path)
+            return
+        }
+        for(let neighbor of graph[node]){
+            dfs(neighbor,path.concat([neighbor]))
+        }
+    }
+
+    dfs(0,[0])
+    return res
+};
