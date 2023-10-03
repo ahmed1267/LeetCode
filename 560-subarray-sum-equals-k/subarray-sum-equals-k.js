9cfa95bbdecc56c6a2628b002355f36b96fb5649
ahmed1267 <ahmedadel837@gmail.com>
2023-10-03 03:02:36 +0300

    Time: 63 ms (95.74%) | Memory: 50.2 MB (76.38%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -4,15 +4,26 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    let map ={}
-    map[0]=1
-    let pSum=0
-    let res=0
-    for(let i=0;i<nums.length;i++){
-        pSum+=nums[i]
-        diff=pSum-k
-        if(map[diff]) res+=map[diff]
-        map[pSum]? map[pSum]+=1 : map[pSum]=1
+    // let map ={}
+    // map[0]=1
+    // let pSum=0
+    // let res=0
+    // for(let i=0;i<nums.length;i++){
+    //     pSum+=nums[i]
+    //     diff=pSum-k
+    //     if(map[diff]) res+=map[diff]
+    //     map[pSum]? map[pSum]+=1 : map[pSum]=1
+    // }
+    // return res
+    let map = new Map();
+    let pSum = 0;
+    let res = 0;
+    map.set(0,1);
+    for (let i=0;i<nums.length;i++) {
+        pSum += nums[i];
+        if (map.has(pSum-k)) res += map.get(pSum-k);
+        if (map.has(pSum)) map.set(pSum, map.get(pSum)+1);
+        else map.set(pSum, 1);
     }
-    return res
+    return res;
 };
