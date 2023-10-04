7ed65d849dfc29e852d0fcbaebb3cb8b4a278a21
ahmed1267 <ahmedadel837@gmail.com>
2023-10-04 19:11:37 +0300

    Time: 38 ms (99.85%) | Memory: 43.1 MB (26.25%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let left=0
-    let right=numbers.length-1
-    while(right!=left){
-        let sum=numbers[left]+numbers[right]
-        if(sum==target) return [left+1,right+1]
-        else if(sum>target) right--
-        else if(sum<target) left++
+    let l=0
+    let r=numbers.length-1
+    while(l!=r){
+        const sum= numbers[l]+numbers[r]
+        if(sum== target) return [l+1,r+1]
+        else if(sum<target) l++
+        else if(sum>target) r--
     }
 };
