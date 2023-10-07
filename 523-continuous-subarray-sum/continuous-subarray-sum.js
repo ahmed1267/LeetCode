ccc00c2b47e4ea339b014c118116b4eb86c93c2a
ahmed1267 <ahmedadel837@gmail.com>
2023-10-07 20:48:19 +0300

    Time: 113 ms (28.77%) | Memory: 66 MB (47.40%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.js
+++ 523-continuous-subarray-sum/continuous-subarray-sum.js
@@ -4,35 +4,35 @@
  * @return {boolean}
  */
 var checkSubarraySum = function (nums, k) {
-    // const n= nums.length;
-    // if(n<2){
-    //     return false;
-    // }
-    // const map = new Map();
-    // map.set(0,-1);
-    // let sum =0;
-    // for(let i=0;i<n;++i){
-    //     sum+=nums[i];
-    //     sum=sum%k;
-    //     if(map.has(sum)&& i-map.get(sum)>1){
-    //         return true;
-    //     }else if(!map.has(sum)){
-    //         map.set(sum,i);
-    //     }
-    // }
-    // return false;
-    let n =nums.length
-    if(n<2) return false;
-    let map={}
-    map[0]=-1
-    let pSum=0
-    for(let i=0;i<n;i++){
-        pSum+=nums[i]
-        rem=pSum%k
-        if(map[rem]!= undefined){
-            if(i-map[rem]>1) return true
-        }
-        if(map[rem]==undefined) map[rem]=i
+    const n= nums.length;
+    if(n<2){
+        return false;
     }
-    return false
+    const map = new Map();
+    map.set(0,-1);
+    let sum =0;
+    for(let i=0;i<n;++i){
+        sum+=nums[i];
+        sum=sum%k;
+        if(map.has(sum)&& i-map.get(sum)>1){
+            return true;
+        }else if(!map.has(sum)){
+            map.set(sum,i);
+        }
+    }
+    return false;
+    // let n =nums.length
+    // if(n<2) return false;
+    // let map={}
+    // map[0]=-1
+    // let pSum=0
+    // for(let i=0;i<n;i++){
+    //     pSum+=nums[i]
+    //     rem=pSum%k
+    //     if(map[rem]!= undefined){
+    //         if(i-map[rem]>1) return true
+    //     }
+    //     if(map[rem]==undefined) map[rem]=i
+    // }
+    // return false
 }
