ead507e1b02bb8fd88998037be1cdf0df3fbf6a1
ahmed1267 <ahmedadel837@gmail.com>
2023-10-07 21:32:55 +0300

    Time: 55 ms (77.29%) | Memory: 46.4 MB (22.10%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -4,22 +4,20 @@
  * @return {number}
  */
 var minSubArrayLen = function (target, nums) {
-
-    
-    let minL = Infinity
-    let rightEdge = 0
-    let leftEdge = 0
-    let sum = nums[0]
-    while (leftEdge <= rightEdge && rightEdge < nums.length) {
-        if (sum >= target) {
-            minL = Math.min(minL, rightEdge - leftEdge + 1)
-            sum -= nums[leftEdge]
-            leftEdge++
-        } else {
-            rightEdge++
-            sum += nums[rightEdge]
+    let minL=Infinity
+    let l=0
+    let r=0
+    let sum=nums[0]
+    while(l<=r && r<nums.length){
+        if(sum>=target){
+            minL=Math.min(minL,r-l+1)
+            sum-=nums[l]
+            l++
+        }else{
+            r++
+            sum+=nums[r]
         }
-
     }
-    return minL== Infinity? 0 : minL
+
+    return minL== Infinity? 0: minL
 };
