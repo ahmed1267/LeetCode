ca82af84fd04fba6ee936ad092a9d5fc87103e09
ahmed1267 <ahmedadel837@gmail.com>
2023-10-11 20:41:58 +0300

    Time: 74 ms (86.75%) | Memory: 46.5 MB (63.56%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.js
@@ -0,0 +1,20 @@
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function(tasks, n) {
+    res=tasks.length
+    let freq=new Array(26).fill(0)
+    for(let task of tasks){
+        freq[task.charCodeAt(0)-'A'.charCodeAt(0)]+=1
+    }
+    freq.sort((a,b)=> b-a)
+    let idle=(freq[0]-1)*n
+
+    for(let i=1;i<freq.length && freq[i]>0;i++){
+        idle-=Math.min(freq[i],(freq[0]-1))
+        
+    }
+    return idle<=0? res : res+idle
+}
