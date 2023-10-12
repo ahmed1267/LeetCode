a9fbdaadc860111f9b63b25e5a641ffb3c8d9aef
ahmed1267 <ahmedadel837@gmail.com>
2023-10-12 20:55:20 +0300

    Time: 54 ms (78.50%) | Memory: 44.4 MB (34.24%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,21 +4,34 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
-    let arr = []
-    for (let i = 0; i < nums1.length; i++) {
-        let oldL = arr.length
-        let curr = nums2.find(number => number === nums1[i])
-        let index = nums2.indexOf(curr)
-        for (let j = index + 1; j < nums2.length; j++) {
-            if (nums2[j] > curr) {
-                arr.push(nums2[j])
-                break;
-            }
+    // let arr = []
+    // for (let i = 0; i < nums1.length; i++) {
+    //     let oldL = arr.length
+    //     let curr = nums2.find(number => number === nums1[i])
+    //     let index = nums2.indexOf(curr)
+    //     for (let j = index + 1; j < nums2.length; j++) {
+    //         if (nums2[j] > curr) {
+    //             arr.push(nums2[j])
+    //             break;
+    //         }
 
+    //     }
+    //     if (oldL === arr.length) {
+    //         arr.push(-1)
+    //     }
+    // }
+    // return arr
+
+    let map = new Map()
+    let stack= []
+    for(let i=nums2.length-1;i>=0;i--){
+        while(stack.length&&stack[stack.length-1]<nums2[i]){
+            stack.pop()
         }
-        if (oldL === arr.length) {
-            arr.push(-1)
-        }
+        let n=stack.length-1
+        map.set(nums2[i],stack[n]> nums2[i] ? stack[n] : -1)
+        stack.push(nums2[i])
     }
-    return arr
+
+    return nums1.map(num => map.get(num))
 };
