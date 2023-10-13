df94daeb660344d2c0231822a31abfe30abff9ab
ahmed1267 <ahmedadel837@gmail.com>
2023-10-14 02:28:53 +0300

    Time: 81 ms (87.22%) | Memory: 46.3 MB (79.74%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -13,30 +13,29 @@ var findMedianSortedArrays = function(nums1, nums2) {
         max=nums1
     }
     let l=0
-    let r=min.length
-    while(l<=r){
-        let mid1= Math.floor((l+r)/2)
-        let mid2= Math.floor((((min.length)+(max.length)+1)/2) - mid1)
+    let r=min.length-1
+    let half=Math.floor((min.length+max.length)/2)
 
-        let maxL1= (mid1==0) ? -Infinity : min[mid1-1]
-        let minR1= (mid1==min.length) ? Infinity : min[mid1]
+    while(true){
+        let i=Math.floor((l+r)/2)
+        let j=half-i-2
 
-        let maxL2= (mid2==0) ? -Infinity : max[mid2-1]
-        let minR2= (mid2==max.length) ? Infinity : max[mid2]
-
-        if(maxL1 <= minR2 && maxL2 <= minR1){
-            if((((min.length)+(max.length))%2)==0){
-                return ((Math.max(maxL1, maxL2)+ Math.min(minR1,minR2))/2)
-            } else {
-                return Math.max(maxL1, maxL2)
+        let leftA= i>=0 && i<min.length ? min[i] : -Infinity
+        let rightA= (i+1)<min.length ? min[i+1] : Infinity
+        let leftB= j>=0 && j<max.length? max[j] : -Infinity
+        let rightB= (j+1)< max.length ? max[j+1] : Infinity
+        if(leftA<= rightB && leftB<= rightA){
+            if(((min.length+max.length)%2)==0){
+                return ((Math.max(leftA,leftB)+ Math.min(rightA,rightB))/2)
+            }else{
+                return Math.min(rightB,rightA)
             }
-        }else if (maxL1>minR2){
-            r=mid1-1
+        }else if(leftA>rightB){
+            r=i-1
         }else{
-            l=mid1+1
+            l=i+1
         }
     }
-    return -1
 
     // let arr=[]
     // let i=0
