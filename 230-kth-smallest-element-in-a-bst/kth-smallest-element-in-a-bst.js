a425cc6a42db5983a7327e36f451e4bd358f36de
ahmed1267 <ahmedadel837@gmail.com>
2023-10-22 02:14:15 +0300

    Time: 67 ms (41.00%) | Memory: 48.1 MB (66.07%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.js
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.js
@@ -12,14 +12,29 @@
  * @return {number}
  */
 var kthSmallest = function(root, k) {
-    let arr =[]
-  var inOrder= function(node){
-      if(node){  
-      let left = inOrder(node.left)
-      arr.push(node.val)
-      let right= inOrder(node.right)
-      }
-  } 
-  inOrder(root)
-  return arr[k-1]
+//     let arr =[]
+//   var inOrder= function(node){
+//       if(node){  
+//       let left = inOrder(node.left)
+//       arr.push(node.val)
+//       let right= inOrder(node.right)
+//       }
+//   } 
+//   inOrder(root)
+//   return arr[k-1]
+let n=0
+let stack=[]
+let curr=root
+while(stack.length || curr ){
+    while(curr){
+        stack.push(curr)
+        curr=curr.left
+    }
+    curr=stack.pop()
+    n+=1
+    if(n==k){
+        return curr.val
+    }
+    curr=curr.right
+}
 };
