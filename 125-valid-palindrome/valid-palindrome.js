3f20cf5d5808437c4cc486f7543aba02de4f9f8b
ahmed1267 <ahmedadel837@gmail.com>
2023-11-26 11:52:47 +0200

    Time: 57 ms (83.23%) | Memory: 43.9 MB (90.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,21 +2,15 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function (s) {
-
-    let str = s.replace(/[^a-z0-9]/gi, "").toLowerCase()
-    if (!str) {
-        return true
-    }
-
-    let rightEdge = str.length - 1
-    let leftEdge = 0
-    while (rightEdge - leftEdge >= 1) {
-        if (str[rightEdge] != str[leftEdge]) {
-            return false
-        }
-        rightEdge--
-        leftEdge++
+var isPalindrome = function(s) {
+    s=s.match(/[A-Za-z0-9]+/g)
+    if(s) s=s.join('').toLowerCase()
+    else return true
+    let l=0,r=s.length-1
+    while(l<r){
+        if(s[l]!=s[r]) return false
+        l+=1
+        r-=1
     }
     return true
-}
+};
