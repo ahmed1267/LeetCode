b214ae34349da8c64e6b33992218efa259dafa68
ahmed1267 <ahmedadel837@gmail.com>
2023-11-26 13:16:19 +0200

    Time: 55 ms (75.38%) | Memory: 44.2 MB (37.41%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,13 +11,38 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if(!list1) return list2
-    else if(!list2) return list1
-    else if(list1.val<=list2.val){
-        list1.next=mergeTwoLists(list1.next,list2)
-        return list1
-    }else {
-        list2.next=mergeTwoLists(list1,list2.next)
-        return list2
+    // if(!list1) return list2
+    // else if(!list2) return list1
+    // else if(list1.val<=list2.val){
+    //     list1.next=mergeTwoLists(list1.next,list2)
+    //     return list1
+    // }else {
+    //     list2.next=mergeTwoLists(list1,list2.next)
+    //     return list2
+    //     }
+
+    let ans=new ListNode()
+    let ptr=ans
+    while(list1&&list2){
+        if(list1.val<=list2.val){
+            ans.next=new ListNode(list1.val)
+            list1=list1.next
+            ans=ans.next
+        }else{
+            ans.next=new ListNode(list2.val)
+            list2=list2.next
+            ans=ans.next
         }
+    }
+    while(list1){
+        ans.next=new ListNode(list1.val)
+        ans=ans.next
+        list1=list1.next
+    }
+    while(list2){
+        ans.next=new ListNode(list2.val)
+        ans=ans.next
+        list2=list2.next
+    }
+    return ptr.next
 }
