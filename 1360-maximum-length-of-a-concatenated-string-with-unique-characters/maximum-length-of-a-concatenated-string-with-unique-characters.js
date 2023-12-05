46fc59dfd5ce62d39165a7caa677a6bfa39c8738
ahmed1267 <ahmedadel837@gmail.com>
2023-12-06 00:40:24 +0200

    Time: 718 ms (26.72%) | Memory: 79.8 MB (17.24%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} arr
+ * @return {number}
+ */
+var maxLength = function(arr) {
+    function dfs(i, str, arr){
+        if(i== arr.length) return isValid(str) ? str.length : 0
+        let take= dfs(i+1, str + arr[i], arr)
+        let leave = dfs(i+1, str,arr)
+        return Math.max(take,leave)
+    }
+
+    function isValid(str){
+        let map={}
+        for(let char of str){
+            if(map[char]) return false
+            map[char]= true
+        }
+        return true
+    }
+    return dfs(0,'',arr)
+};
