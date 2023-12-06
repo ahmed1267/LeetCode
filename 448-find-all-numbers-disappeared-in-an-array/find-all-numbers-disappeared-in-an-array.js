70e381a754696bff4ac5b914dd44631f59f3e04a
ahmed1267 <ahmedadel837@gmail.com>
2023-12-07 01:14:39 +0200

    Time: 77 ms (89.22%) | Memory: 49.8 MB (83.29%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+    let res=[]
+    for(let i=0;i<nums.length;i++){
+      index=Math.abs(nums[i])-1
+      if(nums[index]>0) nums[index]*=-1
+    }
+    for(let i=0;i<nums.length;i++){
+      if(nums[i]>0) res.push(i+1)
+    }
+    return res
+};
