894a7fd0e57d5a1e791b7b3fbb8c322065e7d0a8
ahmed1267 <ahmedadel837@gmail.com>
2023-12-07 22:19:11 +0200

    Time: 84 ms (34.45%) | Memory: 49.9 MB (37.94%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.js
@@ -0,0 +1,36 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var  numIslands= function(grid){
+    let count=0
+
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j]=='1'){
+                count+=1
+                bfs(grid,i,j)
+            }
+        }
+    }
+    return count
+}
+
+function bfs(grid,i,j){
+    let q=[[i,j]]
+    while(q.length){
+        let curr=q.shift()
+        let currRow=curr[0]
+        let currCol=curr[1]
+        let rows=[1,0,0,-1]
+        let cols=[0,1,-1,0]
+        if(currRow<0||currCol<0||currRow>=grid.length||currCol>=grid[0].length) continue;
+        if(grid[currRow][currCol]=='0') continue
+        if(grid[currRow][currCol]=='1'){
+            grid[currRow][currCol]='0'
+        }
+        for(let k=0;k<4;k++){
+            q.push([currRow+rows[k],currCol+cols[k]])
+        }
+    }
+}
