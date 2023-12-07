b14b0ed846a15287348734c9bab76d9a28420a1f
ahmed1267 <ahmedadel837@gmail.com>
2023-12-08 01:59:18 +0200

    Time: 58 ms (74.29%) | Memory: 50.5 MB (9.89%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -3,6 +3,7 @@
  * @return {string}
  */
 var largestOddNumber = function(num) {
+  if (parseInt(num.slice(-1)) % 2 == 1) return num;
   num=num.split('')
   for(let i=num.length-1;i>=0;i--){
     if(!(parseInt(num[i])%2==1)){
