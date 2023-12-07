98b4e4e28e3e850ca1dea006e51981fa9db671a5
ahmed1267 <ahmedadel837@gmail.com>
2023-12-07 15:37:22 +0200

    Time: 74 ms (5.16%) | Memory: 45.6 MB (5.63%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function(n) {
+    let res=Array.from({length:n-1}, (_,index)=> index+1)
+    console.log(res)
+    let last= res.reduce((partial,a)=> partial+a,0)
+    res.push(last*=-1)
+    return res
+};
