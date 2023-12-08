1ea6b813af3c524d434d8ae7a752a6af308ab403
ahmed1267 <ahmedadel837@gmail.com>
2023-12-08 18:23:04 +0200

    Time: 224 ms (30.82%) | Memory: 47.1 MB (32.08%) - LeetSync

---
+++ 454-4sum-ii/4sum-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+var fourSumCount = function(nums1, nums2, nums3, nums4) {
+    let map = new Map()
+    let count=0
+    nums3.forEach(c=> {
+        nums4.forEach(d=> {
+            if(!map.has(c+d)) map.set(c+d,0)
+            map.set(c+d,map.get(c+d)+1)
+        })
+    })
+    nums1.forEach(a=>{
+        nums2.forEach(b=>{
+            if(map.has(-(a+b)))
+            count+=map.get(-(a+b))
+        })
+    })
+    return count
+};
