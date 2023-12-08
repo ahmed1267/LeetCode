e718c9b2481d252ea1893f52bbb3474ae9f7bb7c
ahmed1267 <ahmedadel837@gmail.com>
2023-12-08 17:07:04 +0200

    Time: 77 ms (52.92%) | Memory: 44.1 MB (69.34%) - LeetSync

--- 18-4sum/4sum.js
+++ 18-4sum/4sum.js
@@ -7,32 +7,28 @@ var fourSum = function(nums, target) {
     let res=[]
     let quad=[]
     nums=nums.sort((a,b)=> a-b)
-    var rec= function(k,start,target){
-        if(k!=2){
-            for(let i=start;i<nums.length-k+1;i++){
-                if(i> start && nums[i] == nums[i-1]) continue;
+    function Nsum(N, start, target){
+        if(N!=2){
+            for(let i=start; i<nums.length-N+1;i++){
+                if(i> start && nums[i]== nums[i-1]) continue;
                 quad.push(nums[i])
-                rec(k-1,i+1,target-nums[i])
-                quad.pop()
+                Nsum(N-1,i+1,target-nums[i])
+                quad.pop(nums[i])
 
-                
             }
             return
         }
-        let l=start
-        let r=nums.length-1
+        let l = start , r=nums.length-1
         while(l<r){
-            if(nums[l]+ nums[r]>target)r-=1
-            else if(nums[l]+ nums[r]<target)l+=1
-            else{
-
-                res.push(quad.concat(nums[l],nums[r]))
-                l+=1
-                while(l<r && nums[l]==nums[l-1]) l+=1
+            if((nums[l]+nums[r])> target) r-=1
+            else if((nums[l]+nums[r])< target) l+=1
+            else if(nums[l] +nums[r]== target) {
+                res.push(quad.concat([nums[l],nums[r]]))
+                l+=1    
+                while(l<r && nums[l]== nums[l-1]) l+=1
             }
         }
     }
-
-    rec(4,0,target)
+    Nsum(4,0,target)
     return res
 };
