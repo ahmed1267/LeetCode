bd88e79604d747c1a860a2bff3a5011a5829afc9
ahmed1267 <ahmedadel837@gmail.com>
2023-12-09 21:47:47 +0200

    Time: 67 ms (58.56%) | Memory: 45 MB (64.77%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,31 +4,29 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
-    const map = {}
-    const res = []
-    const heap = new MaxPriorityQueue()
+    // const map = {}
+    // const res = []
+    // const heap = new MaxPriorityQueue()
 
-    for (const n of nums) map[n] = map[n] + 1 || 1
+    // for (const n of nums) map[n] = map[n] + 1 || 1
 
-    for (const [key, count] of Object.entries(map)) {
-        heap.enqueue(key, count)
-        
-        // while (heap.size() > k) heap.dequeue()
-    }
+    // for (const [key, count] of Object.entries(map)) {
+    //     heap.enqueue(key, count)
+    // }
     
-    for (let i = 0; i < k; i++) res.push(heap.dequeue().element)
+    // for (let i = 0; i < k; i++) res.push(heap.dequeue().element)
 
-    return res
-    // let map =new Map()
-    // let res=[]
-    // for(let i=0;i<nums.length;i++){
-    //     if(!map.has(nums[i])) map.set(nums[i],0)
-    //     map.set(nums[i],map.get(nums[i])+1)
-    // }
-    // map= new Map([...map].sort((a,b)=>b[1]-a[1]))
-    // let itr=map.keys()
-    // while(res.length!=k){
-    //     res.push(itr.next().value)
-    // }
     // return res
+    let map =new Map()
+    let res=[]
+    for(let i=0;i<nums.length;i++){
+        if(!map.has(nums[i])) map.set(nums[i],0)
+        map.set(nums[i],map.get(nums[i])+1)
+    }
+    map= new Map([...map].sort((a,b)=>b[1]-a[1]))
+    let itr=map.keys()
+    while(res.length!=k){
+        res.push(itr.next().value)
+    }
+    return res
 };
