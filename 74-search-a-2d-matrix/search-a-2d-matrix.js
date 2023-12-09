ed9187eea8eb5efd93915c88cd38ee852dbc8995
ahmed1267 <ahmedadel837@gmail.com>
2023-12-09 21:55:35 +0200

    Time: 59 ms (18.43%) | Memory: 42.3 MB (33.65%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -6,12 +6,12 @@
 var searchMatrix = function (matrix, target) {
     let n= matrix[0].length
     let m= matrix.length
-    var BS = function (matrix, target, left, right) {
-        if (left > right) return false
-        let mid = Math.floor((left + (right - left) / 2))
+    var BS = function (matrix, target, up, down) {
+        if (up > down) return false
+        let mid = Math.floor((up + (down - up) / 2))
         if (target <= matrix[mid][n - 1] && target >= matrix[mid][0]) return matrix[mid]
-        else if (target > matrix[mid][n - 1]) return BS(matrix, target, mid + 1, right)
-        else if (target < matrix[mid][0]) return BS(matrix, target, left, mid - 1)
+        else if (target > matrix[mid][n - 1]) return BS(matrix, target, mid + 1, down)
+        else if (target < matrix[mid][0]) return BS(matrix, target, up, mid - 1)
 
     }
     let arr = BS(matrix, target, 0, m - 1)
