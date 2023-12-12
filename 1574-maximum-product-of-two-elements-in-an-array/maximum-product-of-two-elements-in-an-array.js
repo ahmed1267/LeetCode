2073bba78b9b923858f025a32fe1d1c2f575d591
ahmed1267 <ahmedadel837@gmail.com>
2023-12-12 19:49:43 +0200

    Time: 49 ms (85.53%) | Memory: 42.2 MB (77.26%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
@@ -3,20 +3,7 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    if(nums.length==2) return (nums[0]-1) * (nums[1]-1)
-    let max=0,index1,index2
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]>max){
-            max=nums[i]
-            index1=i
-        }
-    }
-    max=0
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]>=max && i!= index1){ 
-            max=nums[i]
-            index2=i    
-        }
-    }
-    return (nums[index1]-1) * (nums[index2]-1)
+    let num1=nums.splice(nums.indexOf(Math.max(...nums)),1)
+    let num2=Math.max(...nums)
+    return (num1-1)*(num2-1)
 };
