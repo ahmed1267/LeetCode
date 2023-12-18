7fb1244fca22506b1b621821f3cb31283a8c4fa6
ahmed1267 <ahmedadel837@gmail.com>
2023-12-18 02:45:12 +0200

    Time: 70 ms (81.27%) | Memory: 44.4 MB (86.34%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.js
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.js
@@ -3,7 +3,17 @@
  * @return {number}
  */
 var maxProductDifference = function(nums) {
-    nums.sort((a,b)=> a-b)
-    return (nums[nums.length-2]*nums[nums.length-1])-(nums[0]*nums[1])
+    let max1=nums.indexOf(Math.max(...nums))
+    let min1=nums.indexOf(Math.min(...nums))
+    if(max1==min1) return 0
+    let bufferMax=-Infinity
+    let bufferMin=Infinity
+    for(let i=0;i<nums.length;i++){
+        if(i==max1 || i==min1) continue;
+        if(nums[i]>bufferMax) bufferMax=nums[i]
+        if(nums[i]<bufferMin) bufferMin=nums[i]
+    }
+
+    return (nums[max1]*bufferMax)-(nums[min1]*bufferMin)
 
 };
