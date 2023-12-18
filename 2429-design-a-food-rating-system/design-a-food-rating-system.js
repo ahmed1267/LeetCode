f97d52453531b016c3862c779dee83cdb39c2e4f
ahmed1267 <ahmedadel837@gmail.com>
2023-12-18 02:27:59 +0200

    Time: 2575 ms (9.09%) | Memory: 93 MB (100.00%) - LeetSync

---
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string[]} foods
+ * @param {string[]} cuisines
+ * @param {number[]} ratings
+ */
+var FoodRatings = function(foods, cuisines, ratings) {
+    this.foods=foods
+    this.cuisines=cuisines
+    this.ratings=ratings
+};
+
+/** 
+ * @param {string} food 
+ * @param {number} newRating
+ * @return {void}
+ */
+FoodRatings.prototype.changeRating = function(food, newRating) {
+    for(let i=0;i<this.foods.length;i++){
+        if(food==this.foods[i]) this.ratings[i]=newRating
+    }
+};
+
+/** 
+ * @param {string} cuisine
+ * @return {string}
+ */
+FoodRatings.prototype.highestRated = function(cuisine) {
+    let highest=0
+    let name=[]
+    for(let i=0;i<this.cuisines.length;i++){
+        if(this.cuisines[i]==cuisine && this.ratings[i]>=highest){
+            if(this.ratings[i]>highest){
+                highest=this.ratings[i]
+                name=[this.foods[i]]
+            }else{
+                name.push(this.foods[i])
+            }
+        }
+    }
+    name.sort()
+    return name[0]
+};
+
+/** 
+ * Your FoodRatings object will be instantiated and called as such:
+ * var obj = new FoodRatings(foods, cuisines, ratings)
+ * obj.changeRating(food,newRating)
+ * var param_2 = obj.highestRated(cuisine)
+ */
