a5346b2e4569020f4bc3784521a94b551f139a6b
ahmed1267 <ahmedadel837@gmail.com>
2023-12-18 02:53:52 +0200

    Time: 50 ms (99.30%) | Memory: 44.5 MB (86.34%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.js
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var maxProductDifference = function(nums) {
-    let max1=nums.indexOf(Math.max(...nums)) // index of first max
-    let min1=nums.indexOf(Math.min(...nums)) // index of first min
-    if(max1==min1) return 0 // if the max = min then the whole array is the same number
-    let bufferMax=-Infinity
-    let bufferMin=Infinity
+    let max1I=nums.indexOf(Math.max(...nums)) // index of first max
+    let min1I=nums.indexOf(Math.min(...nums)) // index of first min
+    if(max1I==min1I) return 0 // if the max = min then the whole array is the same number
+    let max2=-Infinity
+    let min2=Infinity
     for(let i=0;i<nums.length;i++){
-        if(i==max1 || i==min1) continue;
-        if(nums[i]>bufferMax) bufferMax=nums[i]
-        if(nums[i]<bufferMin) bufferMin=nums[i]
+        if(i==max1I || i==min1I) continue;
+        if(nums[i]>max2) max2=nums[i]
+        if(nums[i]<min2) min2=nums[i]
     }
 
-    return (nums[max1]*bufferMax)-(nums[min1]*bufferMin)
+    return (nums[max1I]*max2)-(nums[min1I]*min2)
 
 };
