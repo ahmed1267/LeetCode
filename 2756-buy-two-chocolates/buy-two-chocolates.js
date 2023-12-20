70212e6866395ccfc83180ad790f72033dbc62af
ahmed1267 <ahmedadel837@gmail.com>
2023-12-21 00:41:40 +0200

    Time: 86 ms (60.22%) | Memory: 46.2 MB (75.46%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.js
+++ 2756-buy-two-chocolates/buy-two-chocolates.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 var buyChoco = function(prices, money) {
-    prices.sort((a,b)=> a-b)
-    if(prices[0]+prices[1]<=money) return money-(prices[0]+prices[1])
-    else return money
+    let curr=money
+    let min=-1
+    let l=0,r=1
+    while(r<prices.length){
+        if(prices[l]<money){
+            curr-=prices[l]
+            while(r<prices.length){
+                if(prices[r]<=curr){
+                    curr-=prices[r]
+                    min=Math.max(min,curr)
+                    curr+=prices[r]
+                }
+                r+=1
+            }
+        }
+        curr=money
+        l+=1
+        r=l+1
+    }
+    return min>=0? min : money
 };
