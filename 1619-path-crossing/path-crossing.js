1bf244c13ec1a8ff118d577fe238862683799f12
ahmed1267 <ahmedadel837@gmail.com>
2023-12-23 16:17:40 +0200

    Time: 43 ms (93.53%) | Memory: 43.6 MB (14.39%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} path
+ * @return {boolean}
+ */
+var isPathCrossing = function(path) {
+    let set = new Set()
+    point=[0,0]
+    for(let i=0;i<path.length;i++){
+        if(path[i]=='N') point[0]+=1
+        if(path[i]=='S') point[0]-=1
+        if(path[i]=='E') point[1]+=1
+        if(path[i]=='W') point[1]-=1
+        key=`${point[0]},${point[1]}`
+        console.log(key)
+        if(set.has(key) || key== '0,0') return true
+        else set.add(key)
+    }
+
+    return false
+    
+};
