8087b1d05e3d19b17e87bbeca683731dec9a0ccc
ahmed1267 <ahmedadel837@gmail.com>
2023-12-24 11:43:10 +0200

    Time: 51 ms (82.76%) | Memory: 42.6 MB (57.76%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
@@ -4,31 +4,22 @@
  */
 var minOperations = function(s) {
     if(s.length==1) return 0
-    let count=0, count1=0
-    let s2=s.split('')
-    let s1=s.split('')
-    if(s1[0]=='1')s1[0]='0'
-    else s1[0]='1'
-    for(let i=1;i<s2.length;i++){
-        if(s2[i]==s2[i-1] && s2[i]=='0') {
-            count+=1
-            s2[i]='1'
-        }
-        if(s2[i]==s2[i-1] && s2[i]=='1') {
-            s2[i]='0'
-            count+=1
-        }
-    }
-        for(let i=1;i<s1.length;i++){
-        if(s1[i]==s1[i-1] && s1[i]=='0') {
-            count1+=1
-            s1[i]='1'
-        }
-        if(s1[i]==s1[i-1] && s1[i]=='1') {
-            s1[i]='0'
-            count1+=1
-        }
-    }
-    return Math.min(count,count1+1)
+    let pre=s[0]
+    let count1=count(s,pre)
+    let count2= count(s,pre=='0'? '1' : '0')+1
+    return Math.min(count2,count1)
     
 };
+
+function count(s,pre){
+    let count=0
+    for(let i=1;i<s.length;i++){
+        if(s[i]==pre){
+            count+=1
+            pre= pre == '0'? '1' : '0'
+        }else{
+            pre=s[i]
+        }
+    }
+    return count
+}
