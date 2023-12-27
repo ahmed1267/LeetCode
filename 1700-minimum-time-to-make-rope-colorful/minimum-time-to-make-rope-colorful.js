974d9c59815d7e871898e54bf9e5ba388db4c1cd
ahmed1267 <ahmedadel837@gmail.com>
2023-12-27 21:57:52 +0200

    Time: 176 ms (8.16%) | Memory: 54.2 MB (12.25%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.js
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.js
@@ -11,7 +11,7 @@ var minCost = function(colors, neededTime) {
         if(colors[i]==colors[i-1]){
             if(neededTime[i]<neededTime[i-1]){
                 time+=neededTime[i]
-                neededTime.splice(i,1,neededTime[i-1])
+                neededTime[i]=neededTime[i-1]
             }else{
                 time+=neededTime[i-1]
             }
