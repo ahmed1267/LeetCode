d8c7575b783a71bd335207327247ab3ba8392ee2
ahmed1267 <ahmedadel837@gmail.com>
2024-01-01 17:06:09 +0200

    Time: 134 ms (8.57%) | Memory: 44.7 MB (60.00%) - LeetSync

--- 455-assign-cookies/assign-cookies.js
+++ 455-assign-cookies/assign-cookies.js
@@ -4,16 +4,19 @@
  * @return {number}
  */
 var findContentChildren = function(g, s) {
-    g.sort((a,b)=> a-b)
-    s.sort((a,b)=> a-b)
     let res=0
-    for(let i=0;i<g.length;i++){
-        for(let j=0;j<s.length;j++){
-            if(g[i]<=s[j]){
-                s[j]=0
-                res+=1
-                break;
-            }
+    if(s.length==0) return 0
+    s.sort((a,b)=> a-b)
+    g.sort((a,b)=> a-b)
+    let cookieI=s.length-1
+    let childI=g.length-1
+    while(cookieI>=0 && childI>=0){
+        if(s[cookieI]>=g[childI]){
+            res+=1
+            cookieI-=1
+            childI-=1
+        }else{
+            childI-=1
         }
     }
     return res
