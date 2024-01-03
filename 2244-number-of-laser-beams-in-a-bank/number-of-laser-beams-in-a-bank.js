995495d9beb5e285d8b8cd98f22b5e72dba9bad1
ahmed1267 <ahmedadel837@gmail.com>
2024-01-03 16:34:57 +0200

    Time: 94 ms (47.06%) | Memory: 52.2 MB (38.82%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.js
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.js
@@ -3,28 +3,18 @@
  * @return {number}
  */
 var numberOfBeams = function(bank) {
-  let m=bank[0].length , n=bank.length
-  let map=new Array(n).fill(0)
-  let res=0
-  for(let i=0;i<n;i++){
-      for(let j=0; j<m;j++){
-          if(bank[i][j]==1) map[i]+=1
-      }
-  }  
-    let l=0, r=1
-    while(r<n && l<n){
-        if(map[l]==0){
-            l+=1
-            r=l+1
-            continue;
-        }
-        if(map[r]==0){
-            r+=1
-            continue;
-        }
-        res+=map[l]*map[r]
-        l=r
-        r+=1
+    let prev=0
+    let total=0
+
+    const calculate = (s) => {
+        return s.split('').reduce((count,c)=> count+parseInt(c),0)
     }
-    return res
+
+    for(let row of bank){
+        let curr= calculate(row)
+        if(curr==0) continue;
+        total+=curr*prev
+        prev=curr
+    }
+    return total
 };
