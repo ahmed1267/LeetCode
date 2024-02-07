2a4fd9122733f20a6ece513218e7bf8d96d19b5b
ahmed1267 <ahmedadel837@gmail.com>
2024-02-07 19:22:45 +0200

    Time: 86 ms (37.76%) | Memory: 55.8 MB (26.07%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    let map={}
+    let res=[]
+    for(let i=0;i<s.length;i++){
+        if(!map[s[i]])map[s[i]]=0
+        map[s[i]]+=1
+    }
+    let sorted=Object.entries(map).sort((a,b)=> b[1]-a[1])
+    for(let i=0;i<sorted.length;i++){
+        while(map[sorted[i][0]]>0){
+            res.push(sorted[i][0])
+            map[sorted[i][0]]-=1
+        }
+    }
+
+    return res.join('')
+};
