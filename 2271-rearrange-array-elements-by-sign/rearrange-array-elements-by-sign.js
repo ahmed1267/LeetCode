a34e3d8c1aef8840f9d02e2ad05e292e6ac5899a
ahmed1267 <ahmedadel837@gmail.com>
2024-02-14 20:29:51 +0200

    Time: 332 ms (26.02%) | Memory: 92.1 MB (81.91%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -5,17 +5,14 @@
 var rearrangeArray = function(nums) {
  let left=0, right=0
  let res=[]
- for(let i=0;i<nums.length;i++){
+ for(let i=0;left<nums.length && right<nums.length && i<nums.length;i++){
      while(nums[left]<0 && left<nums.length) left++
-     if(nums[left]>0){
          res.push(nums[left])
          left+=1
-         }
+
      while(nums[right]>0 && right<nums.length)right++
-     if(nums[right]<0){
          res.push(nums[right])
          right+=1
-     }
  }
  return res
 };
