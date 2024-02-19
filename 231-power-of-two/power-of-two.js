d2a66c9c68bd14c0badc86ef077b0a9e9a4058da
ahmed1267 <ahmedadel837@gmail.com>
2024-02-20 01:23:57 +0200

    Time: 85 ms (18.88%) | Memory: 53.9 MB (6.40%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,5 +3,20 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    return (n>0 && (n&n-1)==0)
+   var divideByTwo = function(n){
+       if(n>=2){
+           n=n/2
+       }else{
+           return n
+       }
+       return divideByTwo(n)
+   }
+   const number = divideByTwo(n)
+   if (n<=0) {
+        return false
+    } else if(Number.isInteger(number)) {
+        return true
+    }else{
+        return false
+    }
 };
