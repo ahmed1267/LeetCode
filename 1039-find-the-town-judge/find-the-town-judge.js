7022ce258ce6d7bcc9af95f1afb9b505b43849eb
ahmed1267 <ahmedadel837@gmail.com>
2024-02-23 02:13:55 +0200

    Time: 128 ms (12.15%) | Memory: 59 MB (8.35%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.js
+++ 1039-find-the-town-judge/find-the-town-judge.js
@@ -4,14 +4,24 @@
  * @return {number}
  */
 var findJudge = function(n, trust) {
-    let truster= new Array(n+1).fill(0)
-    let trustee= new Array(n+1).fill(0)
-    for(let e of trust){
-        truster[e[1]]+=1
-        trustee[e[0]]+=1
+    if(!trust.length){
+        if(n==1)return 1
+        return -1
     }
-    for(let i=1;i<trustee.length;i++){
-        if(trustee[i]==0 && truster[i]==n-1) return i
+    let sus= new Map()
+    
+    for(let e of trust){
+        if(!sus.has(e[1]))sus.set(e[1],[0,0])
+        if(!sus.has(e[0]))sus.set(e[0],[0,0])
+        tr=sus.get(e[1])
+        tr[1]+=1
+        te=sus.get(e[0])
+        te[0]+=1
+        sus.set(e[1],tr)
+        sus.set(e[0],te)
+    }
+    for(let key of sus.keys()){
+        if(sus.get(key)[0]==0 && sus.get(key)[1]==n-1) return key
     }
     return -1
 };
