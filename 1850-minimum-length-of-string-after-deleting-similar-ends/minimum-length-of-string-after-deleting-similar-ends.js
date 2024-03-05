0e00587a657e54fdbb1a0844ce9877cc243fa218
ahmed1267 <ahmedadel837@gmail.com>
2024-03-05 22:33:52 +0200

    Time: 192 ms (13.51%) | Memory: 59.6 MB (8.11%) - LeetSync

---
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumLength = function(s) {
+    let l=0, r=s.length-1
+    if(s[l]!=s[r]) return s.length
+    while(l<r){
+        let sL=l
+        let sR=r
+        while(s[l]==s[l+1])l++
+        while(s[r]==s[r-1])r--
+        if(s[l]!=s[r]){
+            l=sL
+            r=sR
+            break;
+        };
+        console.log(s[l],s[r])
+        l++
+        r--
+    }
+    console.log(l,r)
+    if(l==r ) return 1
+    if(!(l<r)) return 0
+    return s.substring(l,r+1).length
+};
