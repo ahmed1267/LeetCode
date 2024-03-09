b1ea63c875ac8dccdec7ce4115e9b5131f0db4b4
ahmed1267 <ahmedadel837@gmail.com>
2024-03-09 15:00:54 +0200

    Time: 64 ms (84.69%) | Memory: 56.3 MB (86.99%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function(nums1, nums2) {
+    let l=0, r=0
+    while(l<nums1.length && r<nums2.length){
+        if(nums1[l]>nums2[r]){
+            r++
+        }else if(nums1[l]<nums2[r]){
+            l++
+        }else return nums1[l]
+    }
+    return -1
+};
