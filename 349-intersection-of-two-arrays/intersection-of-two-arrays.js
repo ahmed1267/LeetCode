71b1610cc2f90bc0dfc5a00d2f22387e0fd0e313
ahmed1267 <ahmedadel837@gmail.com>
2024-03-10 22:39:51 +0200

    Time: 60 ms (32.93%) | Memory: 50.8 MB (30.01%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -3,14 +3,14 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersection = function (nums1, nums2) {
-
-    let ans = []
-    
-
-    for (let i = 0; i < nums1.length; i++) {
-        if (nums2.includes(nums1[i])) ans.push(nums1[i])
+var intersection = function(nums1, nums2) {
+    let map =new Set()
+    let res=new Set()
+    for(let i=0;i<nums1.length;i++){
+        if(!map.has(nums1[i]))map.add(nums1[i])
     }
-
-    return ans= [...new Set(ans)]
+    for(let i=0;i<nums2.length;i++){
+        if(map.has(nums2[i]))res.add(nums2[i])
+    }
+    return Array.from(res)
 };
