7660724154ca1f54e6128f9fe325a6bc077fd1be
ahmed1267 <ahmedadel837@gmail.com>
2024-03-17 21:51:35 +0200

    Time: 70 ms (79.71%) | Memory: 55.2 MB (26.81%) - LeetSync

--- 57-insert-interval/insert-interval.js
+++ 57-insert-interval/insert-interval.js
@@ -4,39 +4,21 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    let index
-    let min
-    if(intervals.length==0) return [newInterval]
-    if(newInterval[0]>intervals[intervals.length-1][1]){
-         intervals.push(newInterval)
-         return intervals
-         }
-    for(let i=0;i<intervals.length;i++){
-        if(newInterval[0]<=intervals[i][1]){
-            index=i
-            min=Math.min(newInterval[0], intervals[i][0])
-            while(i<intervals.length){
-                if(newInterval[1]<intervals[i][0]){
-                    intervals.splice(index,i-index,[min, newInterval[1]])
-                    break;
-                }else if(newInterval[1] == intervals[i][0]){
-                      intervals.splice(index,i-index+1,[min, intervals[i][1]])
-                      break;
-                }else if(newInterval[1]> intervals[i][1] && i==intervals.length-1){
-                    
-                    intervals.splice(index,i-index+1,[min, newInterval[1]])
-                      break;
-                }else if(newInterval[1]<intervals[i][1]){
-                     intervals.splice(index,i-index+1,[min, intervals[i][1]])
-                      break;
-                }else if(newInterval[1] == intervals[i][1]){
-                      intervals.splice(index,i-index+1,[min, intervals[i][1]])
-                      break;
-                }
-                i++
-            }
-            break;
-        }
+    let res=[], i=0
+    while(i<intervals.length && intervals[i][1]<newInterval[0]){
+        res.push(intervals[i])
+        i+=1
     }
-   return intervals
+
+    while(i< intervals.length && intervals[i][0] <= newInterval[1]){
+        newInterval=[Math.min(intervals[i][0], newInterval[0]), Math.max(newInterval[1], intervals[i][1])] 
+        i+=1
+    }
+
+    res.push(newInterval)
+    while(i<intervals.length){
+        res.push(intervals[i])
+        i+=1
+    }
+    return res
 };
