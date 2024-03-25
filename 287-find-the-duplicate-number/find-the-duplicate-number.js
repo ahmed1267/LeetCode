dfdaaf3949888479629deb9ed717edfc8be9269b
ahmed1267 <ahmedadel837@gmail.com>
2024-03-25 21:07:05 +0200

    Time: 60 ms (98.06%) | Memory: 57.3 MB (78.59%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -4,9 +4,8 @@
  */
 var findDuplicate = function(nums) {
     
-   let arr= new Array(nums.length-1).fill(0)
-   for(let i =0;i<nums.length;i++){
-    if(arr[nums[i]-1]!=0) return nums[i]
-    arr[nums[i]-1]+=1
-   }
+    for(let i=0;i<nums.length;i++){
+        if(nums[Math.abs(nums[i])-1]<0)return Math.abs(nums[i])
+        nums[Math.abs(nums[i])-1]*=-1
+    }
 };
