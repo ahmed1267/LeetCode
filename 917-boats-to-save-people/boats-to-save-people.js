7aa33867b7c0ca7df099ca2dce4193216d0d5953
ahmed1267 <ahmedadel837@gmail.com>
2024-05-05 00:16:09 +0300

    Time: 582 ms (5.54%) | Memory: 66.6 MB (5.54%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    people= people.sort((a,b)=>a-b)
+    console.log(people)
+    if( people[0]== limit) return people.length
+    let res=0
+    let l=0, r=people.length-1
+    while(l<=r){
+        console.log(people[l], people[r], res)
+        if(people[r]== limit){
+            res+=1
+            r--
+            continue;
+        }
+        if((people[l]+people[r])<=limit){
+            r--
+            l++
+            res++
+        }else if(l==r){ 
+            res +=1
+            r--
+        }
+        else{
+            r--
+            res++
+        }
+    }
+    return res
+};
