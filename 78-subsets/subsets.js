056c07f1d29c98445db4faa30d93cd90f5bae8a9
ahmed1267 <ahmedadel837@gmail.com>
2024-08-26 19:26:56 +0300

    Time: 62 ms (53.56%) | Memory: 53.1 MB (14.27%) - LeetSync

--- 78-subsets/subsets.js
+++ 78-subsets/subsets.js
@@ -6,14 +6,14 @@ var subsets = function(nums) {
     let res=[]
     let sub=[]
     let i=0
-    var rec=(nums,sub,i)=>{
+    var rec=(sub,i)=>{
         if(i==nums.length){
             res.push([...sub])
             return
         }
-        rec(nums,[...sub,nums[i]],i+1)
-        rec(nums,sub,i+1)
+        rec([...sub,nums[i]],i+1)
+        rec(sub,i+1)
     }
-    rec(nums,sub,i)
+    rec(sub,i)
     return res
 };
