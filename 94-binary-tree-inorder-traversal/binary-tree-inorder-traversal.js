cfb0c391ea7737bb60f4bdfdb91052873be3dea7
ahmed1267 <ahmedadel837@gmail.com>
2024-08-26 18:30:43 +0300

    Time: 49 ms (72.68%) | Memory: 49.3 MB (11.34%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -12,11 +12,11 @@
  */
 var inorderTraversal = function(root) {
     let res=[]
-    function dfs(root){
-    if(!root) return
-    dfs(root.left)
-    res.push(root.val)
-    dfs(root.right)
+    function dfs(node){
+        if(!node) return
+        if(node.left)dfs(node.left)
+        res.push(node.val)
+        if(node.right)dfs(node.right)
     }
     dfs(root)
     return res
