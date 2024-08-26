b8fac6b800d0014de0dee9bbe7d26ea9b0b251d2
ahmed1267 <ahmedadel837@gmail.com>
2024-08-26 21:28:06 +0300

    Time: 78 ms (30.76%) | Memory: 53 MB (69.33%) - LeetSync

--- 733-flood-fill/flood-fill.js
+++ 733-flood-fill/flood-fill.js
@@ -6,25 +6,39 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, color) {
-    let q= []
-    q.push([sr,sc])
-    let curr=image[sr][sc]
-    let rowN=[1,0,0,-1]
-    let colN=[0,1,-1,0]
-    image[sr][sc]=color
-    if(color==curr) return image
-    while(q.length){
-        let [row,col]=q.shift()
-        for(let i=0;i<4;i++){
-            let rowDiff=row+rowN[i]
-            let colDiff=col+colN[i]
-            if(rowDiff<0||colDiff<0||rowDiff>=image.length||colDiff>=image[0].length) continue;
+    // let q= []
+    // q.push([sr,sc])
+    // let curr=image[sr][sc]
+    // let rowN=[1,0,0,-1]
+    // let colN=[0,1,-1,0]
+    // image[sr][sc]=color
+    // if(color==curr) return image
+    // while(q.length){
+    //     let [row,col]=q.shift()
+    //     for(let i=0;i<4;i++){
+    //         let rowDiff=row+rowN[i]
+    //         let colDiff=col+colN[i]
+    //         if(rowDiff<0||colDiff<0||rowDiff>=image.length||colDiff>=image[0].length) continue;
             
-            if(image[rowDiff][colDiff]==curr){
-                image[rowDiff][colDiff]=color
-                q.push([rowDiff,colDiff])
-            }
+    //         if(image[rowDiff][colDiff]==curr){
+    //             image[rowDiff][colDiff]=color
+    //             q.push([rowDiff,colDiff])
+    //         }
+    //     }
+    // }
+    // return image
+    let rows=[1,0,0,-1]
+    let cols=[0,1,-1,0]
+    let originalColor=image[sr][sc]
+    let start=image[sr][sc]
+    function rec(image,rows,cols,i,j,color){
+        if(i<0 ||j<0 ||i>image.length-1 || j>image[0].length-1|| image[i][j]==color|| image[i][j]!=start) return
+        image[i][j]=color
+
+        for(let k=0;k<4;k++){
+            rec(image,rows,cols,i+rows[k], j+cols[k],color)
         }
     }
+    rec(image,rows,cols,sr,sc,color)
     return image
 };
