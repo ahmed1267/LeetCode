4fb1550dbf179e0eba09ad562e4d0fa395bb825b
ahmed1267 <ahmedadel837@gmail.com>
2024-12-09 15:41:16 +0200

    Time: 0 ms (100.00%) | Memory: 49.6 MB (10.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
-    var BS = function(nums,target,left,right){
-        if(left>right)return -1
-        let mid= left+Math.floor((right-left)/2)
-        if(nums[mid]==target) return mid
-        if(nums[left]<=nums[mid]){
-            if(target<nums[mid]&&target>=nums[left]) return BS(nums,target,left,mid-1)
-            else return BS(nums,target,mid+1,right)
-        }else{
-            if(target>nums[mid]&&target<=nums[right]) return BS(nums,target,mid+1,right)
-            else return BS(nums,target,left,mid-1)
-        }
+ function BS(nums,target,left,right){
+    if(left>right) return -1
+    let mid= left+Math.floor((right-left)/2)
+    if(target==nums[mid]) return mid
+    if(nums[left]<=nums[mid]){
+        if(target>=nums[left]&& target<nums[mid]) return BS(nums,target,left,mid-1)
+        else return BS(nums,target,mid+1,right)
+    }else{
+        if(target<=nums[right] && target>nums[mid]) return BS(nums,target,mid+1,right)
+        else return BS(nums,target,left,mid-1)
+    }
+
     }
     return BS(nums,target,0,nums.length-1)
 };
